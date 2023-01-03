8cb566e900a2e69338a6f0734537b1cce605fa4b
thao.ttnguyen <thao.ttnguyen@localhost>
2023-01-03 11:07:03 +0530

    Added testcase of record and playback on Katalon webste

--- Test Suites/TestSuite Allego.ts
+++ Test Suites/TestSuite Allego.ts
@@ -5,10 +5,17 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>0</numberOfRerun>
+   <numberOfRerun>3</numberOfRerun>
    <pageLoadTimeout>30</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
-   <rerunImmediately>false</rerunImmediately>
+   <rerunImmediately>true</rerunImmediately>
    <testSuiteGuid>b8936406-dd2e-4774-804b-9c6eeca898b3</testSuiteGuid>
+   <testCaseLink>
+      <guid>2273a4f6-5cf5-4f79-b6fe-2fe68a52f9ac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Record and Playback - Allego</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
